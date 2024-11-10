36781bfe46abb2a7a82cfc3001df4dff2d6a687c
maifeeulasad <maifeeulasad@gmail.com>
2024-11-10 19:56:01 +0600

    [data]: altererd header data;

--- src/home/data.js
+++ src/home/data.js
@@ -1,48 +1,51 @@
 export const header = [
   {
-    title: '产品',
+    title: 'Platform',
     children: [
       {
-        title: '云凤蝶', desc: '移动建站平台', img: 'https://gw.alipayobjects.com/zos/rmsportal/fLPzRmwAurHkPDVfHHiQ.svg', link: 'https://fengdie.alipay-eco.com/intro', top: '2px',
+        title: 'COSOJ',
+        desc: 'Self-Hostable & Open Source',
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/fLPzRmwAurHkPDVfHHiQ.svg',
+        link: 'https://fengdie.alipay-eco.com/intro', 
+        top: '2px',
       },
     ],
   },
   {
-    title: '设计体系',
+    title: 'Principles',
     children: [
       {
-        title: '设计价值观', desc: 'Design Values', img: 'https://gw.alipayobjects.com/zos/rmsportal/zMeJnhxAtpXPZAUhUKJH.svg', link: 'https://ant.design/docs/spec/values-cn',
+        title: 'Innovative',
+        desc: 'Open for ideas and funding open source contributions',
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/zMeJnhxAtpXPZAUhUKJH.svg',
+         link: 'https://ant.design/docs/spec/values-cn',
       },
       {
-        title: '视觉', desc: 'Visual', img: 'https://gw.alipayobjects.com/zos/rmsportal/qkNZxQRDqvFJscXVDmKp.svg', link: 'https://ant.design/docs/spec/colors-cn',
+        title: 'Open Source',
+        desc: 'No vendor lock-in or tracking. Open source from day one.',
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/qkNZxQRDqvFJscXVDmKp.svg',
+        link: 'https://ant.design/docs/spec/colors-cn',
       },
       {
-        title: '可视化', desc: 'Visualisation', img: 'https://gw.alipayobjects.com/zos/rmsportal/MrUQjZNOJhYJCSZZuJDr.svg', link: 'https://antv.alipay.com/zh-cn/vis/index.html',
+        title: 'Metrics Driven',
+        desc: 'Easy to visualize and track performance',
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/MrUQjZNOJhYJCSZZuJDr.svg',
+        link: 'https://antv.alipay.com/zh-cn/vis/index.html',
       },
     ],
   },
   {
-    title: '技术方案',
+    title: 'Flexibility',
     children: [
       {
-        title: 'Ant Design', desc: '蚂蚁 UI 体系', img: 'https://gw.alipayobjects.com/zos/rmsportal/ruHbkzzMKShUpDYMEmHM.svg', link: 'https://ant.design',
-      },
-      {
-        title: 'AntV', desc: '蚂蚁数据可视化解决方案', img: 'https://gw.alipayobjects.com/zos/rmsportal/crqUoMinEgjMeGGFAKzG.svg', link: 'https://antv.alipay.com',
-      },
-      {
-        title: 'Egg', desc: '企业级 Node 开发框架', img: 'https://gw.alipayobjects.com/zos/rmsportal/nEEwwpmNVihZimnBAtMf.svg', link: 'https://eggjs.org',
+        title: 'All Platforms & Devices',
+        desc: 'Web, Mobile, Desktop',
+        img: 'https://gw.alipayobjects.com/zos/rmsportal/fLPzRmwAurHkPDVfHHiQ.svg',
+        link: 'https://fengdie.alipay-eco.com/intro', 
+        top: '2px',
       },
     ],
-  },
-  {
-    title: '关于',
-    children: [
-      {
-        title: '蚂蚁金服体验科技专栏', desc: '探索极致用户体验与最佳工程实践', img: 'https://gw.alipayobjects.com/zos/rmsportal/VsVqfjYxPTJaFbPcZqMb.svg', link: 'https://zhuanlan.zhihu.com/xtech',
-      },
-    ],
-  },
+  }
 ];
 
 export const banner = [
