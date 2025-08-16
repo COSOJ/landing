2ea2694188b06c8bcdb100b26f970271c4102e0a
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-16 17:25:35 +0600

    [refactor]: moved footer to a seperate directory;

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -7,7 +7,7 @@ import Page1 from './page1/Page1';
 import Page2 from './page2/Page2';
 import Page3 from './page3/Page3';
 import Page4 from './page4/Page4';
-import Footer from './Footer';
+import Footer from './footer/Footer';
 import './static/style';
 
 
