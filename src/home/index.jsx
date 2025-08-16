12c9bf073a7493267a3ddbf5b0a81815824806f7
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-16 17:25:59 +0600

    [refactor]: moved banner to a seperate directory;

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { enquireScreen } from 'enquire-js';
 import Header from './header/Header';
-import Banner from './Banner';
+import Banner from './banner/Banner';
 import Page1 from './page1/Page1';
 import Page2 from './page2/Page2';
 import Page3 from './page3/Page3';
