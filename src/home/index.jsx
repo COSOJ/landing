1dd5799ad371a92e777e7b0533bf11659343ca57
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-08-16 17:24:03 +0600

    [refactor]: moved page4 to a seperate directory;

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -6,7 +6,7 @@ import Banner from './Banner';
 import Page1 from './page1/Page1';
 import Page2 from './page2/Page2';
 import Page3 from './page3/Page3';
-import Page4 from './Page4';
+import Page4 from './page4/Page4';
 import Footer from './Footer';
 import './static/style';
 
